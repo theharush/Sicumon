43f533b06b183fedc2bf5fc6d96c115f141e0174
Eyal Harush <theharush@gmail.com>
2019-01-01 13:51:27 +0200

    bug fiixes #1

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+// require node modules
 const express = require("express");
+
+// initialize express
 const app = express();
 
+// set routes
 app.get("/", (req, res) => {
   res.send({ hi: " there" });
 });
+
+// attach react client
+if (process.env.NODE_ENV === "production") {
+  // serve up production assets
+  app.use(express.static("client/build"));
+
+  // serve up the index.html for any other route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
